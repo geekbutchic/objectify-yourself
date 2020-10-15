d60ee6983601279bb61516f01236927924769f04
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2020-10-15 11:32:04 -0400

    updated soluttions

--- main.js
+++ main.js
@@ -3,17 +3,18 @@ const myCodeImmersivesObj = {
 };
 
 // Set the value of property `objectify` to be "yourself".
-myCodeImmersivesObj.objectify = "yourself";
+myCodeImmersivesObj.objectify = 'yourself';
 
 // Add the new property `isAnExcellentObject` and set it to `true`.
-myCodeImmersivesObj.isAnExcellentObject = "true";
+myCodeImmersivesObj.isAnExcellentObject = 'true';
 
 // Add the property `term` and set it to 1.
 myCodeImmersivesObj.term = 1;
 
 // Now add 1 to the value of your object's `term` property and put that new value back in your object's `term`.
-myCodeImmersivesObj.term = 2;
-
+myCodeImmersivesObj.term = myCodeImmersivesObj.term + 1;
+// myCodeImmersivesObj.term += 1;
+// myCodeImmersivesObj.term++;
 // Now add a new property called `currentScore` and set it to 3.
 myCodeImmersivesObj.currentScore = 3;
 
@@ -21,14 +22,15 @@ myCodeImmersivesObj.currentScore = 3;
 myCodeImmersivesObj.cumulativeScore = 50;
 
 // Now add your `currentScore` and your `cumulativeScore` and put it back in `cumulativeScore`.
-myCodeImmersivesObj.cumulativeScore = 53;
+myCodeImmersivesObj.cumulativeScore = myCodeImmersivesObj.cumulativeScore + myCodeImmersivesObj.currentScore
+myCodeImmersivesObj.cumulativeScore
 
 
 // Now add a property called `I am exactly this cool` (you'll need bracket notation!) and set it to `soooooo cool`.
-myCodeImmersivesObj['I am exactly this cool'] = 'soooooo cool'
+myCodeImmersivesObj['I am exactly this cool'] = 'soooooo cool';
 
 // Now make a new EMPTY object that represents yourself.
- const mySelf = {}
+const mySelf = {};
 
 // Add a `firstName` property and set it to a string.
 mySelf.firstName = 'Sonny',
@@ -37,23 +39,35 @@ mySelf.firstName = 'Sonny',
 mySelf.lastName = 'Valenzuela',
 
 // Add a `fullName` property and set it to the name properties put together with a space string in the middle.
-mySelf.fullName = 'Sonny Valenzuela',
+mySelf.fullName = mySelf.firstName + ' ' + mySelf.lastName;
 
 // Add an `age` property and set it to a number.
 mySelf.age = '34'
 
 // Add an address property and set it to an empty array.
-address = {}
-
+mySelf.address = []
+// square brackets for array
+mySelf;
 // Now push three strings into that array.
-const mySelf = {firstName, lastName, age}
+
+mySelf.address.push('cellist')
+mySelf.address.push('investor')
+mySelf.address.push('italian')
+mySelf;
 
 // Now make a new object, but this time we'll pre-populate it, not set it in any lines below it. In other words, the properties should be set the same way I initially set the `codeImmersivesObject` on line 1. It can be called whatever you want and can have whatever properties you want, as long as they include at least one string value, boolean value, number value, and array of numbers. The important thing here is: this should all be done within the brackets of the object, not below the place where the object is declared.
-const myJeep = {
-myJeep.color: 'black'
-myJeep.year: '1996'
-myJeep.lifted: 'true'
-};
+
+const dinosaur = {
+  species: 'T - Rex',
+  extinct: true,
+  teeth: 1000,
+  weight: 10000,
+  foods: [
+    'Triceratops',
+    'Stegosaurus',
+    'Pteranodon',
+  ],
+}
 
 // As a stretch goal, pre-populate the object with at least one property using bracket notation as well. 
 
